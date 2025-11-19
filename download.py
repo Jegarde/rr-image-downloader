848a79d830f880aaffb52cbd1c1bcbdcb4f2d683
Jegarde <jesse.tapani.nieminen@gmail.com>
2025-11-19 16:22:33 +0200

    uvloop

--- download.py
+++ download.py
@@ -2,6 +2,7 @@ import aiohttp
 import os
 import asyncio
 import aiofiles
+import requests
 from tqdm.asyncio import tqdm_asyncio
 from secret import API_KEY
 
@@ -23,23 +24,32 @@ FOLDER = "images"
 CONCURRENCY = 50 
 TIMEOUT = aiohttp.ClientTimeout(total=30)
 
+# id -> name
+ROOMS = {
+
+}
+
+# id -> name
+PLAYERS = {
+
+}
+
+PLAYER_IDS = set()
 
 async def download_all(session, image_urls):
     os.makedirs(FOLDER, exist_ok=True)
 
     sem = asyncio.Semaphore(CONCURRENCY)
-    tasks = [download_image(session, url, sem) for url in image_urls]
+    tasks = [download_image(session, img_tuple, sem) for img_tuple in image_urls]
     results = await tqdm_asyncio.gather(*tasks)
 
     image_count = len(image_urls)
     print(f"\nDownloaded {sum(results)} / {image_count} images")
 
 
-async def download_image(session, url, sem):
-    filename = url.split("img.rec.net/")[-1]
-    if "." not in filename:
-        filename += ".png"
-    filepath = os.path.join(FOLDER, filename)
+async def download_image(session, img_tuple, sem):
+    url = img_tuple[0]
+    filepath = img_tuple[1]
 
     async with sem:
         try:
@@ -69,19 +79,50 @@ async def gather_image_urls(session, id) -> list:
             total_image_count += image_count
             skip += total_image_count
 
-            for img in resp:
-                image_urls.append(f"https://img.rec.net/{img['ImageName']}")
+            for image_object in resp:
+                image_urls.append(create_image_tuple(image_object))
 
             if image_count != MAX_IMAGES_PER_RESPONSE:
                 break
 
     return image_urls
 
+"""
+TUPLE: (image url, filepath)
+"""
+def create_image_tuple(image_object):
+    image_name = image_object["ImageName"]
+    creation_date = image_object["CreatedAt"].split("T")[0]
+
+    filename = f"{creation_date} [{image_object['Id']}] {image_name}"
+
+
+    filename = add_png_extension_if_missing(filename)
+    filepath = os.path.join(FOLDER, filename)
+
+    return (
+        "https://img.rec.net/" + image_name,
+        filepath
+    )
+
+def fetch_player_name(player_id):
+    if player_id in PLAYERS:
+        return PLAYERS[player_id]
+
+    requests.get()
+
+
+
+def add_png_extension_if_missing(filename):
+    if "." not in filename:
+        filename += ".png"
+    return filename
+
 
 async def main():
     session = aiohttp.ClientSession(headers=HEADERS)
 
-    image_urls = await gather_image_urls(session, 9085)
+    image_urls = await gather_image_urls(session, 205981)
     image_count = len(image_urls)
     print("Images found:", image_count)
     prompt = input("Want to download all? (y/n) > ")
